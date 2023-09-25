ff28722973bacca77b8d59208e81804091650dd5
timot <timothylinziqi@gmail.com>
2023-09-24 20:15:55 -0400

    final version

--- csf_assign02/c_wcfuncs.c
+++ csf_assign02/c_wcfuncs.c
@@ -21,12 +21,12 @@
 // Note that the character values should be treated as
 // being unsigned (in the range 0..255)
 uint32_t wc_hash(const unsigned char *w) {
-  uint32_t hash_code = 5381;
-  while(*w) {
-    hash_code = hash_code * 33 + (uint32_t)*w;
-    w++;
-  }
-  return hash_code;
+    uint32_t hash_code = 5381;
+    while(*w) {
+        hash_code = hash_code * 33 + (uint32_t)*w;
+        w++;
+    }
+    return hash_code;
 }
 
 // Compare two strings lexicographically. Return
@@ -41,25 +41,23 @@ uint32_t wc_hash(const unsigned char *w) {
 // "hi" would compare as less than "high".
 int wc_str_compare(const unsigned char *lhs, const unsigned char *rhs) {
     while (*lhs && *rhs) {
-        if (*lhs < *rhs) return -1;
-        if (*lhs > *rhs) return 1;
+        if (*lhs != *rhs) {
+            return *lhs - *rhs;
+        }
         lhs++;
         rhs++;
     }
-
-    if (*lhs) return 1;   // lhs is longer
-    if (*rhs) return -1;  // rhs is longer
-    return 0;
+    return *lhs - *rhs;
 }
 
 // Copy NUL-terminated source string to the destination buffer.
 void wc_str_copy(unsigned char *dest, const unsigned char *source) {
-  while (*source != '\0' && *source) {
-    *dest = *source;
-    dest++;
-    source++;
-  }
-  *dest = '\0';
+    while (*source != '\0' && *source) {
+        *dest = *source;
+        dest++;
+        source++;
+    }
+    *dest = '\0';
 }
 
 // Return 1 if the character code in c is a whitespace character,
@@ -74,19 +72,19 @@ void wc_str_copy(unsigned char *dest, const unsigned char *source) {
 //   '\f'
 //   '\v'
 int wc_isspace(unsigned char c) {
-  if (c == ' ' || c == '\t' || c == '\r' || c == '\n' || c == '\f' || c == '\v') {
-    return 1;
-  }
-  return 0;
+    if (c == ' ' || c == '\t' || c == '\r' || c == '\n' || c == '\f' || c == '\v') {
+        return 1;
+    }
+    return 0;
 }
 
 // Return 1 if the character code in c is an alphabetic character
 // ('A' through 'Z' or 'a' through 'z'), 0 otherwise.
 int wc_isalpha(unsigned char c) {
-  if (((int)c >= 65 && (int)c <= 90) || ((int)c >= 97 && (int)c <= 122)) {
-    return 1;
-  }
-  return 0;
+    if (((int)c >= 65 && (int)c <= 90) || ((int)c >= 97 && (int)c <= 122)) {
+        return 1;
+    }
+    return 0;
 }
 
 // Read the next word from given input stream, storing
@@ -101,37 +99,37 @@ int wc_isalpha(unsigned char c) {
 // MAX_WORDLEN characters, then only the first MAX_WORDLEN
 // characters in the sequence should be stored in the array.
 int wc_readnext(FILE *in, unsigned char *w) {
-  int ch;
-  int count = 0;
-  if (!in || !w) return 0;
+    int ch;
+    int count = 0;
+    if (!in || !w) return 0;
 
-  while ((ch = fgetc(in)) != EOF && wc_isspace(ch));
+    while ((ch = fgetc(in)) != EOF && wc_isspace(ch));
 
-  if (ch == EOF) return 0;
+    if (ch == EOF) return 0;
 
-  ungetc(ch, in);
+    ungetc(ch, in);
 
-  while ((ch = fgetc(in)) != EOF && !wc_isspace(ch) && count < MAX_WORDLEN) {
-    w[count++] = ch;
-  }
+    while ((ch = fgetc(in)) != EOF && !wc_isspace(ch) && count < MAX_WORDLEN) {
+        w[count++] = ch;
+    }
 
-  w[count] = '\0';
+    w[count] = '\0';
 
-  if (count > 0) {
-    return 1;
-  }
-  return 0;
+    if (count > 0) {
+        return 1;
+    }
+    return 0;
 }
 
 // Convert the NUL-terminated character string in the array
 // pointed-to by w so that every letter is lower-case.
 void wc_tolower(unsigned char *w) {
-  while(*w) {
-    if (((int)*w >= 65 && (int)*w <= 90)) {
-      *w = (char)((int)*w+32);
+    while(*w) {
+        if (((int)*w >= 65 && (int)*w <= 90)) {
+            *w = (char)((int)*w+32);
+        }
+        w++;
     }
-    w++;
-  }
 }
 
 // Remove any non-alphaabetic characters from the end of the
@@ -143,15 +141,15 @@ void wc_trim_non_alpha(unsigned char *w) {
     }
     finalDest--;
 
-  unsigned char c;
-  while((c = *finalDest) && finalDest >= w){
-      if(wc_isalpha(c)){
-          break;
-      }
-      finalDest--;
-  }
-  finalDest++;
-  *finalDest = '\0';
+    unsigned char c;
+    while((c = *finalDest) && finalDest >= w){
+        if(wc_isalpha(c)){
+            break;
+        }
+        finalDest--;
+    }
+    finalDest++;
+    *finalDest = '\0';
 }
 
 // Search the specified linked list of WordEntry objects for an object
@@ -167,28 +165,27 @@ void wc_trim_non_alpha(unsigned char *w) {
 // the new node should have its count value set to 0. (It is the caller's
 // job to update the count.)
 struct WordEntry *wc_find_or_insert(struct WordEntry *head, const unsigned char *s, int *inserted) {
-  struct WordEntry *temp = head;
-  
-  while (temp != NULL) {
-      if (wc_str_compare((const unsigned char *)temp->word, (const unsigned char *)s) == 0) {
-          *inserted = 0;
-          temp->count++;
-          return temp;
-      }
-      temp = temp->next;
-  }
+    struct WordEntry *temp = head;
 
-  struct WordEntry *new_entry = (struct WordEntry *)malloc(sizeof(struct WordEntry));
-  wc_str_copy((unsigned char *)new_entry->word, (unsigned const char *)s);
-  new_entry->count = 0;
-  new_entry->next = head;
-  // if (wc_str_compare(new_entry->word,(const unsigned char *)"dominates") == 0 || wc_str_compare(new_entry->word,(const unsigned char *)"sustained") == 0) {
-  //           printf("new %s\nold %s\n",new_entry->word,new_entry->next->word);
+    while (temp != NULL) {
+        if (wc_str_compare((const unsigned char *)temp->word, (const unsigned char *)s) == 0) {
+            *inserted = 0;
+            return temp;
+        }
+        temp = temp->next;
+    }
 
-  //       }
-        
-  *inserted = 1;
-  return new_entry;
+    struct WordEntry *new_entry = (struct WordEntry *)malloc(sizeof(struct WordEntry));
+    wc_str_copy((unsigned char *)new_entry->word, (unsigned const char *)s);
+    new_entry->count = 0;
+    new_entry->next = head;
+    // if (wc_str_compare(new_entry->word,(const unsigned char *)"dominates") == 0 || wc_str_compare(new_entry->word,(const unsigned char *)"sustained") == 0) {
+    //           printf("new %s\nold %s\n",new_entry->word,new_entry->next->word);
+
+    //       }
+
+    *inserted = 1;
+    return new_entry;
 }
 
 // Find or insert the WordEntry object for the given string (s), returning
@@ -199,22 +196,22 @@ struct WordEntry *wc_find_or_insert(struct WordEntry *head, const unsigned char 
 // Returns a pointer to the WordEntry object in the appropriate linked list
 // which represents s.
 struct WordEntry *wc_dict_find_or_insert(struct WordEntry *buckets[], unsigned num_buckets, const unsigned char *s) {
-  uint32_t index = wc_hash(s) % num_buckets;
-  struct WordEntry *temp;
-  int inserted = 0;
-  temp = wc_find_or_insert(buckets[index], s, &inserted);
-  if(inserted == 1) {
-    buckets[index] = temp;
-  }
-  return temp;
+    uint32_t index = wc_hash(s) % num_buckets;
+    struct WordEntry *temp;
+    int inserted = 0;
+    temp = wc_find_or_insert(buckets[index], s, &inserted);
+    if(inserted == 1) {
+        buckets[index] = temp;
+    }
+    return temp;
 }
 
 // Free all of the nodes in given linked list of WordEntry objects.
 void wc_free_chain(struct WordEntry *p) {
-  struct WordEntry *temp;
-  while (p) {
-      temp = p;
-      p = p->next;
-      free(temp);
-  }
+    struct WordEntry *temp;
+    while (p) {
+        temp = p;
+        p = p->next;
+        free(temp);
+    }
 }
--- csf_assign02/c_wcmain.c
+++ csf_assign02/c_wcmain.c
@@ -9,76 +9,76 @@
 // TODO: prototypes for helper functions
 
 int main(int argc, char **argv) {
-  FILE *fp = NULL;
-  // stats (to be printed at end)
-  uint32_t total_words = 0;
-  uint32_t unique_words = 0;
-  unsigned char best_word[MAX_WORDLEN +1] = {'\0'};
-  uint32_t best_word_count = 0;
+    FILE *fp = NULL;
+    // stats (to be printed at end)
+    uint32_t total_words = 0;
+    uint32_t unique_words = 0;
+    unsigned char best_word[MAX_WORDLEN +1] = {'\0'};
+    uint32_t best_word_count = 0;
 
-  unsigned char *w = malloc(MAX_WORDLEN + 1);
+    unsigned char *w = malloc(MAX_WORDLEN + 1);
 
-  struct WordEntry *bucket[HASHTABLE_SIZE] = {NULL};
-  struct WordEntry *temp;
-  if(argc > 1) {
-    fp = fopen(argv[1],"r");
-    if(fp == NULL) {
-      perror("no file\n");
-      return 1;
-    }
-    while(wc_readnext(fp, w)){
-      wc_trim_non_alpha(w);
-      wc_tolower(w);
-      total_words++;
-      temp = wc_dict_find_or_insert(bucket,HASHTABLE_SIZE,w);
-      temp->count++;
-    }
-  } else {
-    //TODO
-    
-    
-  }
-  
-
-  
-  
-  for (int i = 0; i < HASHTABLE_SIZE; i++) { 
-    if (bucket[i] != NULL) {
-      temp = bucket[i];
-      unique_words++;
-        // if (wc_str_compare(temp->word,(const unsigned char *)"dominates") == 0) {
-        //   printf("this\n");
-        // }
-      if (best_word_count < temp->count) {
-        best_word_count = temp->count;
-        wc_str_copy(best_word,temp->word);
-      }
-       while(temp->next != NULL) {
-        temp = temp->next;
-        unique_words++;
-        // if (wc_str_compare(temp->word,(const unsigned char *)"dominates") == 0) {
-        //   printf("this\n");
-        // }
-        if (best_word_count < temp->count) {
-          best_word_count = temp->count;
-          wc_str_copy(best_word,temp->word);
+    struct WordEntry *bucket[HASHTABLE_SIZE] = {NULL};
+    struct WordEntry *temp;
+    if(argc > 1) {
+        fp = fopen(argv[1],"r");
+        if(fp == NULL) {
+            perror("no file\n");
+            return 1;
         }
-      }
-    }
-  }
-  //printf("%d\n\n\n",count);
-  printf("Total words read: %u\n", (unsigned int) total_words);
-  printf("Unique words read: %u\n", (unsigned int) unique_words);
-  printf("Most frequent word: %s (%u)\n", (const char *) best_word, best_word_count);
+        while(wc_readnext(fp, w)){
+            wc_trim_non_alpha(w);
+            wc_tolower(w);
+            total_words++;
+            temp = wc_dict_find_or_insert(bucket,HASHTABLE_SIZE,w);
+            temp->count++;
+        }
+    } else {
+        //TODO
 
-  // TODO: make sure file is closed (if one was opened)
-  // TODO: make sure memory is freed
-  free(w);
-  fclose(fp);
-  for (int i = 0; i < HASHTABLE_SIZE; i++) { 
-    wc_free_chain(bucket[i]);
-  }
-  return 0;
+
+    }
+
+
+
+
+    for (int i = 0; i < HASHTABLE_SIZE; i++) {
+        if (bucket[i] != NULL) {
+            temp = bucket[i];
+            unique_words++;
+            // if (wc_str_compare(temp->word,(const unsigned char *)"dominates") == 0) {
+            //   printf("this\n");
+            // }
+            if (best_word_count < temp->count) {
+                best_word_count = temp->count;
+                wc_str_copy(best_word,temp->word);
+            }
+            while(temp->next != NULL) {
+                temp = temp->next;
+                unique_words++;
+                // if (wc_str_compare(temp->word,(const unsigned char *)"dominates") == 0) {
+                //   printf("this\n");
+                // }
+                if (best_word_count < temp->count) {
+                    best_word_count = temp->count;
+                    wc_str_copy(best_word,temp->word);
+                }
+            }
+        }
+    }
+    //printf("%d\n\n\n",count);
+    printf("Total words read: %u\n", (unsigned int) total_words);
+    printf("Unique words read: %u\n", (unsigned int) unique_words);
+    printf("Most frequent word: %s (%u)\n", (const char *) best_word, best_word_count);
+
+    // TODO: make sure file is closed (if one was opened)
+    // TODO: make sure memory is freed
+    free(w);
+    fclose(fp);
+    for (int i = 0; i < HASHTABLE_SIZE; i++) {
+        wc_free_chain(bucket[i]);
+    }
+    return 0;
 }
 
 // TODO: definitions of helper functions
